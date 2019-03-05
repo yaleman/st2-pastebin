f0b4d457fc4e9522169da32847760fb0f63de526
yaleman <yaleman@ricetek.net>
2019-03-05 15:34:15 +1000

    adding logging to _get_last_time

--- sensors/pastebin_poller.py
+++ sensors/pastebin_poller.py
@@ -115,8 +115,10 @@ class PasteBinPoller(PollingSensor):
 
     def _get_last_time(self):
         """ returns the last timestamp that was processed by the poller """
+        self._logger.debug("PastebinPoller._get_last_time() start")
         if not self._last_time and hasattr(self._sensor_service, 'get_value'):
             self._last_time = self._sensor_service.get_value(name='last_time')
+        self._logger.debug("PastebinPoller._get_last_time() end, returning {}".format(self._last_time))
         return self._last_time
 
     def _set_last_time(self, last_time):
